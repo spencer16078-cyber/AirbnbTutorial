71d8a8d1dc9534c14e5c3d491e1772236a0af95f
Spencer Searle <spencer16078@gmail.com>
2025-09-10 22:31:52 -0600

    Initial Commit

---
+++ AirbnbTutorial/AirbnbTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbTutorialApp.swift
+//  AirbnbTutorial
+//
+//  Created by Spencer Searle on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
