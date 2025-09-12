968829e65cbe0dbdeed17c33024a9a1dced0753e
Spencer Searle <spencer16078@gmail.com>
2025-09-11 22:46:43 -0600

    9 11 Version

---
+++ AirbnbTutorial/Core/Explore/ExploreView.swift
@@ -0,0 +1,36 @@
+//
+//  ExploreView.swift
+//  AirbnbTutorial
+//
+//  Created by Spencer Searle on 9/10/25.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                SearchAndFilterBar()
+                
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { listing in
+                        NavigationLink(value: listing) {
+                            ListingItemView()
+                                .frame(height: 400)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        }
+                    }
+                }
+                .padding()
+            }
+            .navigationDestination(for: Int.self) { listing in
+                Text("Listing detail view...")
+            }
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
